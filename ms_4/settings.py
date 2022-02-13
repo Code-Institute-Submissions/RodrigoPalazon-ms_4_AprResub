2e7afdcc809362006dfec09e39b044cc322f961d
Rodrigo Palazon <palazonbatera@gmail.com>
2022-02-13 14:41:39 +0100

    Fixing Django settings

--- ms_4/settings.py
+++ ms_4/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'c7daef0f-5faa-4b60-a32e-6044963e016e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
