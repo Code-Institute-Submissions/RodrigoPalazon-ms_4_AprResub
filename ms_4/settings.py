a931cf76c44ec3d4d1fcbd4674e8a340bdd334af
Rodrigo Palazon <palazonbatera@gmail.com>
2022-02-26 14:52:59 +0100

    Fix AWS_URL in settings.py

--- ms_4/settings.py
+++ ms_4/settings.py
@@ -205,12 +205,13 @@ if 'USE_AWS' in os.environ:
     # Static and media filesAWS_S3_REGION_NAME
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
+    STYLE_LOCATION = 'css'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/{STYLE_LOCATION}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
