81caa4fffff3757b40f63284295ec2427441c733
Rodrigo Palazon <palazonbatera@gmail.com>
2022-02-26 16:20:30 +0100

    update aws config

--- custom_storages.py
+++ custom_storages.py
@@ -2,13 +2,9 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-# class StaticStorage(S3Boto3Storage):
-    # location = settings.STATICFILES_LOCATION
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
 
 
-# class MediaStorage(S3Boto3Storage):
-    # location = settings.MEDIAFILES_LOCATION
-
-
-print(settings.STATICFILES_LOCATION)
-print(settings.MEDIAFILES_LOCATION)
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
